b92999e033ae6f91cbc939bfd6ae0646e61db45c
Ryan Johnson <rjguitar98@yahoo.com>
2023-02-17 09:04:02 -0500

    Initial Commit

---
+++ au-currencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  au-currencyConverter
+//
+//  Created by Ryan Johnson on 2/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
